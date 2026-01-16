8c73634753ec822bcf667d7ab770d8a482a6bfe1
DeadPS <127458301+XDARKNEST@users.noreply.github.com>
2026-01-16 10:03:23 +0800

    Change countdown deadline time to 12 PM
    Updated countdown deadline to 12 PM on January 18, 2026.

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ imageModal.onclick=e=>{
 };
 
 /* COUNTDOWN */
-const deadline = new Date(2026, 0, 18, 0, 0, 0).getTime();
+const deadline = new Date(2026, 0, 18, 12, 0, 0).getTime();
 const nextDeadline = new Date(2026, 0, 11, 8, 0, 0).getTime();
 
 const cd = document.getElementById("countdown");
