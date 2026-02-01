8e8cb1b7d0a1f9ba1dcac4e562b5aaca8c52ff32
DeadPS <127458301+XDARKNEST@users.noreply.github.com>
2026-02-01 11:20:35 +0800

    Update countdown deadlines in script.js

--- script.js
+++ script.js
@@ -61,8 +61,8 @@ volumeSlider.oninput=()=>{
 };
 
 /* COUNTDOWN */
-const deadline = new Date(2026, 1, 1, 12, 0, 0).getTime();
-const nextDeadline = new Date(2026, 0, 25, 8, 0, 0).getTime();
+const deadline = new Date(2026, 1, 1, 11, 0, 0).getTime();
+const nextDeadline = new Date(2026, 1, 8, 8, 0, 0).getTime();
 setInterval(()=>{
   const now=Date.now();
   const d1=deadline-now;
