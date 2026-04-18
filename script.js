57db5cbdf8e785c97700bd2c0cb336d485755f00
DeadPS <127458301+XDARKNEST@users.noreply.github.com>
2026-04-18 20:48:06 +0800

    Fix countdown deadline date and time

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ volumeSlider.oninput=()=>{
 };
 
 /* COUNTDOWN */
-const deadline = new Date(2026, 2, 22, 12, 0, 0).getTime();
+const deadline = new Date(2026, 3, 22, 28, 0, 0).getTime();
 const nextDeadline = new Date(2026, 1, 8, 8, 0, 0).getTime();
 setInterval(()=>{
   const now=Date.now();
